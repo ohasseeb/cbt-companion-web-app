470a4d60d1f4eeea88bc1b1679c02fe24a8093fb
Obaid Hasseeb <hasseeb.obaid.2016@gmail.com>
2022-01-03 22:21:30 -0800

    got the initial design structuer down for the home page in HTML going to turn into components tomorrow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import logo from "./logo512.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+  <div>
+    <div className="depression-test">
+      <div>Depression Test(PHQ-9)</div>
+      <div className="buttons">
+        <button className="info-button"> i </button>
+        <button> Start Now</button>
+      </div>
+    </div>{" "}
+    {/* ^ end depression Test div*/}
+    {/* starting _|_ Daiuly checkin Div div*/}
+    <div className="daily-checkin">
+      <div className="inline-daily-checkin">
+        <div> Daily Checkin</div>
+        <div> Record Mood, Emotions and journal</div>
+        <button className="start-button"> Start</button>
+      </div>
     </div>
-  );
-}
+    {/* Ending ^ Daily Checkin Div  Div div*/}
+    {/* starting _|_ Thoughttools Div div*/}
+    {/*  */}
+    <div className="CBT-exercise-topic">
+      <div> Thought Tools</div>
+      <br />
+      <div className="thought-tool-group">
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button">
+            {" "}
+            Challenge Automatic Thoughts
+          </label>
+          <button id="thought-tool-exercise-button">
+            {/* <img src={logo} /> */}
+            Place Holder Text
+          </button>
+        </div>
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button"> Prediction</label>
+          <button id="thought-tool-exercise-button">Place Holder Text </button>
+        </div>
+      </div>
+    </div>
+    {/*  */}
+    {/*  */}
+    <div className="CBT-exercise-topic">
+      <div> Goals, Values & Actions</div>
+      <br />
+      <div className="thought-tool-group">
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button"> Goal Setting</label>
+          <button id="thought-tool-exercise-button">Place Holder Text </button>
+        </div>
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button">
+            {" "}
+            Smart Goal Building
+          </label>
+          <button id="thought-tool-exercise-button">Place Holder Text </button>
+        </div>
+      </div>
+    </div>
+    {/*  */}
+    {/*  */}
+    <div className="CBT-exercise-topic">
+      <div> Mindfulness Tools</div>
+      <br />
+      <div className="thought-tool-group">
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button">
+            {" "}
+            Gratitude Checkin
+          </label>
+          <button id="thought-tool-exercise-button">Place Holder Text </button>
+        </div>
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button"> Reflection</label>
+          <button id="thought-tool-exercise-button">Place Holder Text </button>
+        </div>
+      </div>
+    </div>
+    {/*  */}
+    {/*  */}
+    <div className="CBT-exercise-topic">
+      <div> Sleep, Nutrition & Activity</div>
+      <br />
+      <div className="thought-tool-group">
+        <div className="thought-tool-exercise">
+          <label htmlFor="thought-tool-exercise-button"> Log Sleep</label>
+          <button id="thought-tool-exercise-button">Place Holder Text </button>
+        </div>
+      </div>
+    </div>
+    <div></div>
+    {/*  */}
+  </div>
+);
 
 export default App;
+
+/*
+
+
+
+Mindfulness Tools
+  Gratitude Checkin 
+  Reflection
+
+Sleep, Nutrition & Activity
+  Log Sleep
+
+
+*/
