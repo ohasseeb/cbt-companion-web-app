47a14e5c52bd0e35d51d80805b51ee237d65276b
Obaid Hasseeb <hasseeb.obaid.2016@gmail.com>
2022-01-05 12:52:54 -0800

    Fixed the Daily Checkin Styling, getting checked by designer just need to add fonts, going to work on Depression Checklist, have to resize svg

--- src/components/daily-checkin/daily-checkin.component.jsx
+++ src/components/daily-checkin/daily-checkin.component.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./daily-checkin.styles.scss";
 
+import DailycheckinImage from "../../DailyCheckin.png";
+
 const DailyCheckin = () => (
-  <div className="daily-checkin">
-    <div className="inline-daily-checkin">
-      <div className="title"> Daily Checkin </div>
-      <div className="subtitle"> Record Mood, Emotions and journal</div>
-      <button className="start-button"> Start </button>
+  <div className="total">
+    <div className="daily-checkin">
+      <div className=" labels-together">
+        <div className="title"> Daily Checkin </div>
+        <div> Record Mood, Emotions and journal</div>
+
+        <button className="start-button"> Start</button>
+      </div>
+      <img className="img" src={DailycheckinImage} />
     </div>
   </div>
 );
