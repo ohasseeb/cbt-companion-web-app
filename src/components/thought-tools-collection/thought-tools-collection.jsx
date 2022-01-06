cd384053937619be8d7512ea57a784b52348430a
Obaid Hasseeb <hasseeb.obaid.2016@gmail.com>
2022-01-05 16:21:49 -0800

    got images working will makea  pull request tn

--- src/components/thought-tools-collection/thought-tools-collection.jsx
+++ src/components/thought-tools-collection/thought-tools-collection.jsx
@@ -1,23 +1,28 @@
 import ThoughtTool from "../thought-tools/thought-tool.component";
-
+import AutoMaticThoughtImage from "../../ChallengeAutomaticThoughts.png";
+import BrainImage from "../../Brain.png";
 const ThoughtToolCollection = () => (
   <div>
     {" "}
     <ThoughtTool
       topicTitle={"Thought Tools"}
       exerciseTitles={["Challenge Automatic Thoughts", " Prediction"]}
+      image={[AutoMaticThoughtImage, BrainImage]}
     />
     <ThoughtTool
       topicTitle={"Goals Values & Actions"}
       exerciseTitles={["Goal Setting", "Smart Goal Building"]}
+      image={[AutoMaticThoughtImage, BrainImage]}
     />
     <ThoughtTool
       topicTitle={"Mindfulness Tools"}
       exerciseTitles={["Gratitude Checkin", "Reflection"]}
+      image={[AutoMaticThoughtImage, BrainImage]}
     />
     <ThoughtTool
       topicTitle={"Sleep Nutrition & Activity"}
       exerciseTitles={["Log Sleep"]}
+      image={[AutoMaticThoughtImage, BrainImage]}
     />
   </div>
 );
--- src/components/thought-tools/thought-tool.component.jsx
+++ src/components/thought-tools/thought-tool.component.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import "./thought-tool.styles.scss";
 
-const ThoughtTool = ({ topicTitle, exerciseTitles }) => {
+const ThoughtTool = ({ topicTitle, exerciseTitles, image }) => {
+  var iter = 0;
   return (
     <div className="CBT-exercise-topic">
-      <div> {topicTitle} </div>
-      <br />
+      {/* <img src="/static/media/ChallengeAutomaticThoughts.2a9f5f33023c34930dba.png" /> */}
+      <div className="title"> {topicTitle} </div>
+      {/* <br /> */}
 
       <div className="thought-tool-group">
         {exerciseTitles.map((exercise) => (
           <div className="thought-tool-exercise" key={exercise}>
             <label htmlFor="thought-tool-exercise-button"> {exercise}</label>
+            {console.log(iter)}
             <button id="thought-tool-exercise-button">
-              {/* <img src={logo} /> */}
-              Place Holder Text
+              <img src={image[iter]} className="image" />
+              {(iter = iter + 1)}
+              {console.log(iter)}
+              {/* <img src={image} />
+              Place Holder Text */}
             </button>
           </div>
         ))}
